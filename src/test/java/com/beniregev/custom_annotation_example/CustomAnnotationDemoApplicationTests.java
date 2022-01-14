032a41cfe2a5b61b2d4f6b238d0b43d9c50f4b06
binyamin.regev <beniregev@gmail.com>
2022-01-14 13:16:18 +0200

    Refactoring Application and Application Tests classes

--- src/test/java/com/beniregev/custom_annotation_example/CustomAnnotationDemoApplicationTests.java
+++ src/test/java/com/beniregev/custom_annotation_example/CustomAnnotationDemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java8SpringMavenH2RestAppExampleApplicationTests {
+class CustomAnnotationDemoApplicationTests {
 
     @Test
     void contextLoads() {
